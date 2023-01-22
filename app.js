7d4f047119efda3e40b016d937563a2d23b9c122
Jamie Barlow <jamiebarlow@gmail.com>
2023-01-22 14:46:39 +0000

    Add routing for new campground form, and nav links

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Used to parse the req.body
+app.use(express.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
     res.render('home');
 })
@@ -29,13 +32,19 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 })
 
+app.get('/campgrounds/new', async (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async(req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
-})
-
-app.get('/campgrounds/new', async (req, res) => {
-    res.render('campgrounds/new');
 })
 
 app.listen(3000, () => {
