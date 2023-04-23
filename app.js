b38cedac953e3564b464cbba5287e3a334e8c04c
Jamie Barlow <jamiebarlow@gmail.com>
2023-04-23 14:59:07 +0100

    Update route labelling and refactor logging middleware

--- app.js
+++ app.js
@@ -24,13 +24,15 @@ app.use(methodOverride('_method'));
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: true})) // Used to parse the req.body
 
-app.use((req, res, next) => {
+let requestTime = function (req, res, next) {
     let date = Date.now();
     let dateFormatted = new Date(date).toString().slice(4, 24);
     req.requestTime = dateFormatted;
-	console.log("Method:", req.method.toUpperCase(), "Path:", req.path);
+	console.log("Method:", req.method.toUpperCase(), "Path:", req.path, "Time:", req.requestTime);
 	next();
-})
+}
+
+app.use(requestTime);
 
 // Password verification test function
 const verifyPassword = (req, res, next) => {
@@ -44,48 +46,56 @@ const verifyPassword = (req, res, next) => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Routing
+// Routing - home page
 app.get('/', (req, res) => {
     console.log(`request time: ${req.requestTime}`)
     res.render('home');
 })
 
+// Secret password test
 app.get('/secret', verifyPassword, (req, res) => {
 	res.send(`MY SECRET IS: Sometimes I wear headphones in public so I
 	don't have to talk to people`)
 })
 
+// Index of all campgrounds
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
 })
 
+// NEW form for new campgrounds
 app.get('/campgrounds/new', async (req, res) => {
     res.render('campgrounds/new');
 })
 
+// CREATE creating new campgrounds
 app.post('/campgrounds', async(req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
 })
 
+// SHOW route for specific campground
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
 })
 
+// EDIT route
 app.get('/campgrounds/:id/edit', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground });
 })
 
+// UPDATE route
 app.put('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
+// DESTROY route
 app.delete('/campgrounds/:id', async(req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
