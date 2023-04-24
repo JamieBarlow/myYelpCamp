a40cf1d25c843e677dd155c0631ca904141dc411
Jamie Barlow <jamiebarlow@gmail.com>
2023-04-24 16:50:59 +0100

    Fix index.js seed function for populating images. Add BS styling for index page

--- models/campground.js
+++ models/campground.js
@@ -3,6 +3,9 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title: String,
+    image: String,
+    creator: String,
+    username: String,
     price: Number,
     description: String,
     location: String
--- seeds/index.js
+++ seeds/index.js
@@ -19,37 +19,44 @@ db.once("open", () => {
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
-// async function getUnsplashImg() {
-//     const clientID = 'ABhnQtYm5-yuKn5fSfByt85dJgpFmSinNcNQdKE1Dds';
-//     const endpoint = `https://api.unsplash.com/photos/random/?client_id=${clientID}`;
-//     fetch(endpoint)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error('Network response was not ok');
-//             }
-//             return response.json();
-//         })
-//         .then(data => {
-//             // Handle the JSON data here
-//             return data.urls.regular;
-//         })
-//         .catch(error => {
-//             // Handle any errors here
-//             console.error('There was a problem with the fetch operation:', error);
-//         });
-// }
-
+async function getUnsplashImg() {
+    const clientID = 'ABhnQtYm5-yuKn5fSfByt85dJgpFmSinNcNQdKE1Dds';
+    const collection = 483251;
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${clientID}&collections=${collection}`;
+    return await fetch(endpoint)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Handle the JSON data here
+            let imgObject = {};
+            imgObject.url = data.urls.regular;
+            imgObject.creator = data.user.name;
+            imgObject.username = data.user.username;
+            return imgObject;
+        })
+        .catch(error => {
+            // Handle any errors here
+            console.error('There was a problem with the image fetch operation:', error);
+        });
+}
 
 // For removing all existing items / testing
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 40; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
+        const image = await getUnsplashImg();
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'hello',
+            image: image.url,
+            creator: image.creator,
+            username: image.username,
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             price
         })
