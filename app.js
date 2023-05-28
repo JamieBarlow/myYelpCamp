9e2b2dd3eb9c7512829f05192e13515156b69782
Jamie Barlow <jamiebarlow@gmail.com>
2023-05-28 12:43:19 +0100

    Add review delete button and routing

--- app.js
+++ app.js
@@ -145,6 +145,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) 
     // res.send('You made it!!')
 }))
 
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
+    await Review.findByIdAndDelete(req.params.reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
+
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
 })
