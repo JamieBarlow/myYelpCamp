b8c52e2b5013fd4ee9c2b79f7f96aa94e81ac54e
Jamie Barlow <jamiebarlow@gmail.com>
2023-06-30 11:07:03 +0100

    Update register routing and add login form

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const User = require('../models/user');
 const router = express.Router();
+const catchAsync = require('../utils/catchAsync');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
 })
 
-router.post('/register', async (req, res) => {
-    res.send(req.body);
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        req.flash('success', 'Welcome to YelpCamp!');
+        res.redirect('/campgrounds');
+        console.log(registeredUser);
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register')
+    }
+}));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+router.post('/login', (req, res) => {
+
 })
 
 module.exports = router;
