845b1445b10b0f36f6d65c129baee314a0690b5b
Jamie Barlow <jamiebarlow@gmail.com>
2023-06-30 15:44:44 +0100

    Add auto-login on registration

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const Campground = require('../models/campground');
 const Review = require('../models/review');
+const {isLoggedIn} = require('../middleware');
 
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
@@ -18,7 +19,7 @@ const validateReview = (req, res, next) => {
     }
 }
 
-router.post('/', validateReview, catchAsync(async(req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
@@ -29,7 +30,7 @@ router.post('/', validateReview, catchAsync(async(req, res) => {
     // res.send('You made it!!')
 }))
 
-router.delete('/:reviewId', catchAsync(async(req, res) => {
+router.delete('/:reviewId', isLoggedIn, catchAsync(async(req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
     await Review.findByIdAndDelete(req.params.reviewId);
