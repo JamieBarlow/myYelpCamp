d4c9dd2990e73b717b2de9e33d1c274acd302706
Jamie Barlow <jamiebarlow@gmail.com>
2023-07-02 12:00:33 +0100

    Add reviews author, permissions and dynamic visibility based on user authorization

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-const {isLoggedIn, validateReview} = require('../middleware');
+const {isLoggedIn, validateReview, isReviewAuthor} = require('../middleware');
 
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
@@ -10,6 +10,7 @@ const catchAsync = require('../utils/catchAsync');
 router.post('/', isLoggedIn, validateReview, catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
@@ -18,7 +19,7 @@ router.post('/', isLoggedIn, validateReview, catchAsync(async(req, res) => {
     // res.send('You made it!!')
 }))
 
-router.delete('/:reviewId', isLoggedIn, catchAsync(async(req, res) => {
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async(req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
     await Review.findByIdAndDelete(req.params.reviewId);
