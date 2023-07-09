f9aa57947cefc134aa5236024386edddc1dbf3d1
Jamie Barlow <jamiebarlow@gmail.com>
2023-07-09 12:18:01 +0100

    Add image thumbnails with Mongoose virtuals and Cloudinary image transformations API

--- models/campground.js
+++ models/campground.js
@@ -2,17 +2,21 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_200/r_30/');
+})
+
 const CampgroundSchema = new Schema({
     title: {
         type: String,
         required: [true, 'Name cannot be blank']
     },
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     creator: String,
     username: String,
     price: {
