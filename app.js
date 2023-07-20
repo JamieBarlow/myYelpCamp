a15a2297d0d2d095e4267cf893923ea96ab1864e
Jamie Barlow <jamiebarlow@gmail.com>
2023-07-20 18:58:17 +0100

    Add Helmet and Content Security Policy directives

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const User = require('./models/user');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 // Joi validation schemas:
 const { campgroundSchema, reviewSchema } = require('./schemas');
@@ -53,10 +54,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 
-const sessionOptions = { 
+const sessionOptions = {
     name: 'YCSession',
     secret: 'thisisnotagoodsecret',
-    resave: false, 
+    resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
@@ -73,6 +74,57 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use(flash());
+app.use(
+    helmet({
+        // contentSecurityPolicy: false
+    })
+);
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dakgl7s9n/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
 app.use((req, res, next) => {
     console.log(req.query);
     // console.log('Req.session:')
@@ -109,7 +161,7 @@ const verifyPassword = (req, res, next) => {
 
 // Routing
 app.get('/fakeUser', async (req, res) => {
-    const user = new User({email: 'colttt@gmail.com', username: 'colttt'});
+    const user = new User({ email: 'colttt@gmail.com', username: 'colttt' });
     const newUser = await User.register(user, 'fakePassword');
     res.send(newUser);
 })
@@ -132,7 +184,7 @@ app.all('*', (req, res, next) => {
 // Generic error handling
 const genericError = (err, req, res, next) => {
     const { status = 500 } = err;
-    if(!err.message) err.message = 'Something went wrong';
+    if (!err.message) err.message = 'Something went wrong';
     console.log(err.status)
     res.status(status).render('errorPage', { err });
 }
