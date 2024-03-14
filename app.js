3ebbd70a2b373f068153a4f6a81addc72857b098
Jamie Barlow <jamiebarlow@gmail.com>
2024-03-14 11:48:16 +0000

    Update db for test dev

--- app.js
+++ app.js
@@ -21,8 +21,14 @@ const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const MongoStore = require("connect-mongo");
 
-// Access Mongo Atlas DB in production mode, or local db in dev mode
-const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
+// Access Mongo Atlas DB or local test db
+let dbUrl;
+if (process.env.NODE_ENV !== "production") {
+  dbUrl = "mongodb://127.0.0.1:27017/yelp-camp";
+} else {
+  dbUrl = process.env.DB_URL;
+}
+// const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
 
 // Joi validation schemas:
 const { campgroundSchema, reviewSchema } = require("./schemas");
